ef9c96f13a62c73eb80c5202f9a2fea112ea8bad
nekl08 <nekl08@gmail.com>
2018-08-22 19:28:40 +0300

    HW02 Optional change user

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -18,4 +18,11 @@ public class UserServlet extends HttpServlet {
         log.debug("forward to users");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int userId = Integer.valueOf(req.getParameter("userId"));
+        SecurityUtil.setUserId(userId);
+        resp.sendRedirect("meals");
+    }
 }
